d6143619881c6624285dca73fb59ba023c24daca
Dainis Dilevka <mozart_dd@outlook.com>
2025-06-18 20:17:52 +0100

    Refactored, removed hard coded values. Added await chain

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,33 +1,19 @@
-async function getWetherApi(location='Moscow') {
-  try {
-    //* Fetches weather api from visual crossing web server
-    const resolved = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=DUX8WLQUANH4ZKTHFG32ZK63L`
-    );
-    const resolvedData = await resolved.json();
-    return resolvedData;
-  } catch(err) {
-    throw new Error(`Error: ${err}`);
-  }
+export async function getWeatherApi(location) {
+  //* Fetches weather api from visual crossing web server
+  const resolved = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=DUX8WLQUANH4ZKTHFG32ZK63L`);
+  const resolvedData = await resolved.json();
+  return resolvedData;
 }
 
-export async function storeWeatherData(location) {
-  // Array to store wether api data
-  let dataArray = [];
-  try {
-    const data = await getWetherApi(location);
-    const dataObj = {
-      location: data.address,
-      tempF: data.currentConditions.temp,
-      tempC: Math.ceil((data.currentConditions.temp - 32) * 5 / 9),
-      conditions: data.currentConditions.conditions,
-      desc: data.description
-    }
-    // Empty array before push new weather data
-    dataArray = [];
-    dataArray.push(dataObj);
-    return dataArray;
-  } catch(err) {
-    throw new Error(`Error: ${err}`);
+// Formats wether api promise data in to readable obj
+export async function formatWeatherData(apiData) {
+  const data = await apiData;
+  const dataObj = {
+    location: data.address,
+    tempF: data.currentConditions.temp,
+    tempC: Math.ceil((data.currentConditions.temp - 32) * 5 / 9),
+    conditions: data.currentConditions.conditions,
+    desc: data.description
   }
+  return dataObj;
 }
--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -1,18 +1,34 @@
-import { storeWeatherData } from "./api";
+import { formatWeatherData, getWeatherApi } from "./api";
 
 const userInput = document.querySelector('input');
 const btn = document.querySelector('button');
 
-async function displayData() {
+// Receives data as promise obj and displays it on screen trough DOM elements
+async function displayData(stored) {
+  console.log(stored.location);
+}
+
+async function apiChain(input) {
   try {
-    const display = await storeWeatherData(userInput.value);
-    console.log(display);
-  } catch {
-    console.log(await storeWeatherData('Moscow'));
+    const data = await getWeatherApi(input);
+    const stored = await formatWeatherData(data);
+    const display = await displayData(stored);
+    return display;
+  } catch(err) {
+    if (userInput.value === '') {
+      alert('Location\'s name can\'t be empty string.');
+      return err;
+    }
+    while (err) {
+      userInput.value = '';
+      alert('Please write valid name of geographical location.');
+      break;
+    } 
   }
 }
 
 btn.addEventListener('click', (event) => {
   event.preventDefault();
-  displayData();
+  apiChain(userInput.value);
 })
+apiChain('London');
